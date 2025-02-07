9d8fd903ab35b1fb242e953404bd32500f8151af
Prasanth631 <prasanthgolla29@gmail.com>
2025-02-07 22:06:08 +0530

    Change done in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class App
+ * Servlet implementation class App Hi
  */
 @WebServlet("/App")
 public class App extends HttpServlet {
